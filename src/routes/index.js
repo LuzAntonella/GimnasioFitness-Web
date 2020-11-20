2347af2d5b06dcdae0e97dd5d4e5ec3f1fae7219
LuzAntonella <cabrejosseverinoluzantonella@gmail.com>
2020-11-20 00:08:37 -0500

    commitIndex

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const router = express.Router();
-
-router.get('/',(req, res) => {
-    res.render('index');
+const Docente = require('../models/Docente');
+router.get('/',async (req, res) => {
+    await Docente.find({Docente: req.body._id})
+    .then(documentos => {
+        const contexto = {
+            Docente: documentos.map(documento => {
+            return {
+                dni: documento.dni,
+                name: documento.name,
+                apellido: documento.apellido,
+                telefono: documento.telefono,
+                email: documento.email,
+                profesion: documento.profesion,
+                image: documento.imageURL,
+                id: documento._id,
+                
+            }
+          })
+        }
+        res.render('index', {Docente: contexto.Docente}); 
+      });
 });
 
 router.get('/about',(req, res) => {
